875dc0a4c3170220406b85945f0ef1be2198361d
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-10 16:50:42 +0900

    fix

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { CANCEL } from "bdsx/common";
 import { ipfilter } from "bdsx/core";
 import { events } from "bdsx/event";
 
-console.log("[plugin:Anticrasher] allocated");
+console.log("[Anticrasher] allocated", " - mdisprgm".blue);
 
 events.serverOpen.on(() => {
     console.log("[ANTICRASHER] launching", " - mdisprgm".blue);
@@ -27,18 +27,13 @@ events.packetSend(MinecraftPacketIds.LevelSoundEvent).on((pkt, ni) => {
     })();
 });
 
-events.packetSend(MinecraftPacketIds.PlayerAuthInput).on((pkt, ni) => {
-    const mX = pkt.moveX;
-    const mZ = pkt.moveZ;
-    const x = pkt.pos.x;
-    const y = pkt.pos.y;
-    const z = pkt.pos.z;
+events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pkt, ni) => {
     switch (true) {
-        case mX > 1073741824:
-        case mZ > 1073741824:
-        case x > 1073741824:
-        case y > 1073741824:
-        case z > 1073741824:
+        case pkt.moveX > 1073741824:
+        case pkt.moveZ > 1073741824:
+        case pkt.pos.x > 1073741824:
+        case pkt.pos.y > 1073741824:
+        case pkt.pos.z > 1073741824:
             serverInstance.disconnectClient(ni);
             return CANCEL;
         default:
