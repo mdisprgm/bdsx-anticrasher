633c7d5aae085e2b5e9072e6ce91fc9b62083827
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-13 18:29:05 +0900

    FIX

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { serverInstance } from "bdsx/bds/server";
 import { CANCEL } from "bdsx/common";
@@ -17,14 +18,30 @@ events.serverClose.on(() => {
 ipfilter.setTrafficLimit(1024 * 1024);
 ipfilter.setTrafficLimitPeriod(3600);
 
-events.packetSend(MinecraftPacketIds.LevelSoundEvent).on((pkt, ni) => {
-    const sound = pkt.sound;
-    (async () => {
-        if (sound === 0) {
-            serverInstance.disconnectClient(ni);
-            return CANCEL;
-        }
-    })();
+const Sounds = new Map<NetworkIdentifier, NodeJS.Timeout>();
+const SOUNDS_DELAY = 16;
+events.packetBefore(MinecraftPacketIds.LevelSoundEvent).on((pkt, ni) => {
+    if (pkt.sound !== 0) return;
+    if (Sounds.has(ni)) {
+        clearTimeout(Sounds.get(ni)!);
+        Sounds.set(
+            ni,
+            setTimeout(() => {
+                Sounds.delete(ni);
+            }, SOUNDS_DELAY)
+        );
+        serverInstance.disconnectClient(ni);
+        const ip = ni.getAddress().split("|")[0];
+        ipfilter.add(ip, 1073741824);
+        return CANCEL;
+    } else {
+        Sounds.set(
+            ni,
+            setTimeout(() => {
+                Sounds.delete(ni);
+            }, SOUNDS_DELAY)
+        );
+    }
 });
 
 events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pkt, ni) => {
@@ -34,6 +51,8 @@ events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pkt, ni) => {
         case pkt.pos.x > 1073741824:
         case pkt.pos.y > 1073741824:
         case pkt.pos.z > 1073741824:
+            const ip = ni.getAddress().split("|")[0];
+            ipfilter.add(ip, 1073741824);
             serverInstance.disconnectClient(ni);
             return CANCEL;
         default:
