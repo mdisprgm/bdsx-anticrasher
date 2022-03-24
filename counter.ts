997e2a40c436abc94bd31f01c09d5d2fb2fe991a
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-24 18:34:16 +0900

    add counter

---
+++ counter.ts
@@ -0,0 +1,79 @@
+import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
+import { MinecraftPacketIds } from "bdsx/bds/packetids";
+import { serverInstance } from "bdsx/bds/server";
+import { CANCEL } from "bdsx/common";
+import { ipfilter } from "bdsx/core";
+import { events } from "bdsx/event";
+
+events.packetAfter(MinecraftPacketIds.Login).on(async (pkt, ni) => {});
+
+export class Counter {
+    constructor();
+    constructor(delay_limit: number, max_warns: number);
+    constructor(delay_limit?: number, max_warns?: number) {
+        if (delay_limit != null && max_warns != null) {
+            this.delay_limit = delay_limit;
+            this.max_warns = max_warns;
+        }
+    }
+
+    private delay_limit: number = 3;
+    private max_warns: number = 3;
+    private last_map: Map<NetworkIdentifier, number> = new Map();
+    private count_map: Map<NetworkIdentifier, number> = new Map();
+
+    reset(subject: NetworkIdentifier): void {
+        this.count_map.set(subject, 0);
+        this.last_map.set(subject, Date.now());
+    }
+    warn(subject: NetworkIdentifier): void {
+        const old_counts = this.count_map.get(subject);
+
+        this.last_map.set(subject, Date.now());
+        this.count_map.set(subject, (old_counts ?? 0) + 1);
+    }
+    pass(subject: NetworkIdentifier) {
+        this.reset(subject);
+    }
+
+    enter(subject: NetworkIdentifier): CANCEL | void {
+        const last = this.last_map.get(subject);
+        const counts = this.count_map.get(subject);
+        this.warn(subject);
+
+        // 처음이 아니면
+        if (last != null && counts != null) {
+            // 시간 비교해서
+            if (Date.now() - last < this.delay_limit) {
+                // 여러 번 이면
+                if (counts > this.max_warns) {
+                    // 킥
+                    Counter.addBanned(subject);
+                    this.reset(subject);
+                    return CANCEL;
+                }
+                // 시간이 길면 값 초기화
+            } else this.pass(subject);
+        }
+        // 처음이면 값 초기화
+        else this.pass(subject);
+    }
+}
+
+export namespace Counter {
+    events.networkDisconnected.on(async (ni) => {
+        if (Banned.has(ni)) {
+            ipfilter.add(Banned.get(ni)!);
+            Banned.delete(ni);
+        }
+    });
+    const Banned = new Map<NetworkIdentifier, string>();
+    export function addBanned(target: NetworkIdentifier) {
+        if (Banned.has(target)) return;
+
+        const ip = target.getAddress().split("|")[0];
+        if (ip !== "10.10.10.10") Banned.set(target, ip);
+
+        serverInstance.disconnectClient(target, "§cKicked by trying Crasher");
+    }
+}
--- index.ts
+++ index.ts
@@ -1,96 +1,42 @@
-import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { ActorEventPacket } from "bdsx/bds/packets";
-import { serverInstance } from "bdsx/bds/server";
 import { CANCEL } from "bdsx/common";
 import { ipfilter } from "bdsx/core";
 import { events } from "bdsx/event";
+import { Counter } from "./counter";
 
-console.log("[ANTICRASHER] allocated", " - mdisprgm".blue);
-
-events.serverOpen.on(() => {
-    console.log("[ANTICRASHER] launching", " - mdisprgm".blue);
-});
-
-events.serverClose.on(() => {
-    console.log("[ANTICRASHER] closed", " - mdisprgm".blue);
-});
+{
+    console.log("[ANTICRASHER] allocated", " - mdisprgm".blue);
+    events.serverOpen.on(() => {
+        console.log("[ANTICRASHER] launching", " - mdisprgm".blue);
+    });
+    events.serverClose.on(() => {
+        console.log("[ANTICRASHER] closed", " - mdisprgm".blue);
+    });
+}
 
 ipfilter.setTrafficLimit(1024 * 1024);
 ipfilter.setTrafficLimitPeriod(3600);
-const DELAY_LIMIT = 3;
 
-const LAST = new Map<NetworkIdentifier, number>(); //TIME between last and second last chat
-const COUNT = new Map<NetworkIdentifier, number>(); //warning COUNT
+const addBanned = Counter.addBanned;
 
-const Banned = new Map<NetworkIdentifier, string>();
-
-/**
- * Ban client but for the session
- */
-function addBanned(target: NetworkIdentifier) {
-    if (Banned.has(target)) return;
-
-    const ip = target.getAddress().split("|")[0];
-    if (ip !== "10.10.10.10") Banned.set(target, ip);
-
-    serverInstance.disconnectClient(target, "§cKicked by trying Crasher");
-}
-
-events.packetAfter(MinecraftPacketIds.Login).on(async (pkt, ni) => {
-    LAST.set(ni, 0);
-    COUNT.set(ni, 0);
-});
-
-events.networkDisconnected.on(async (ni) => {
-    LAST.delete(ni);
-    COUNT.delete(ni);
-
-    if (Banned.has(ni)) {
-        ipfilter.add(Banned.get(ni)!);
-        Banned.delete(ni);
-    }
-});
+const InvalidSoundCounter = new Counter(3, 3);
+const InvalidFoodCounter = new Counter(3, 3);
 
 // invalid sound events
 {
     events.packetBefore(MinecraftPacketIds.LevelSoundEvent).on((pkt, ni) => {
         if ([12, 26, 35, 42].includes(pkt.sound)) return;
-
-        if (Date.now() - LAST.get(ni)! < DELAY_LIMIT) {
-            const next = COUNT.get(ni)!;
-            COUNT.set(ni, next + 1);
-            if (next > 3) {
-                addBanned(ni);
-            }
-
-            return CANCEL;
-        }
-        COUNT.set(ni, 0);
-        LAST.set(ni, Date.now());
+        return InvalidSoundCounter.enter(ni);
     });
 }
 
 // invalid eating food
 {
-    const FOOD_LAST = new Map<NetworkIdentifier, number>(); //TIME between last and second last chat
-    const FOOD_COUNT = new Map<NetworkIdentifier, number>(); //warning COUNT
-
     events.packetBefore(MinecraftPacketIds.ActorEvent).on((pkt, ni) => {
         const action = pkt.event;
         if (action !== ActorEventPacket.Events.EatingItem) return;
-
-        if (Date.now() - FOOD_LAST.get(ni)! < DELAY_LIMIT) {
-            const next = FOOD_COUNT.get(ni)!;
-            FOOD_COUNT.set(ni, next + 1);
-            if (next > 3) {
-                addBanned(ni);
-            }
-
-            return CANCEL;
-        }
-        FOOD_COUNT.set(ni, 0);
-        FOOD_LAST.set(ni, Date.now());
+        return InvalidFoodCounter.enter(ni);
     });
 }
 
@@ -98,17 +44,18 @@ events.networkDisconnected.on(async (ni) => {
 {
     events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pkt, ni) => {
         switch (true) {
-            case pkt.moveX > 1073741823:
-            case pkt.moveZ > 1073741823:
-            case pkt.pos.x > 1073741823:
-            case pkt.pos.y > 1073741823:
-            case pkt.pos.z > 1073741823:
+            case pkt.moveX > 0x3fffffff:
+            case pkt.moveZ > 0x3fffffff:
+            case pkt.pos.x > 0x3fffffff:
+            case pkt.pos.y > 0x3fffffff:
+            case pkt.pos.z > 0x3fffffff:
                 addBanned(ni);
                 return CANCEL;
             default:
         }
     });
 }
+
 try {
     require("bdsx/../../example_and_test/vulnerabilities");
 } catch {
